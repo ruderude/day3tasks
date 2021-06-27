13eaa1c68d5a234974ebc273b098a4dfb8ed3dd9
ruderude <rude1979@gmail.com>
2021-06-27 04:34:20 +0000

    www

--- app/Http/Controllers/V1/LineController.php
+++ app/Http/Controllers/V1/LineController.php
@@ -32,8 +32,6 @@ class LineController extends Controller
 
         Log::debug(print_r($json,true));
 
-        exit;
-
         $event = $json["events"][0];
         $mid = $event["source"]["userId"];
         $type = $event["message"]["type"] ?? $event["type"];
