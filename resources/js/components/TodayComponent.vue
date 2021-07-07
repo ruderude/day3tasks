77ee66619eeac3395102780a800a4272862b87cd
ruderude <rude1979@gmail.com>
2021-07-07 03:31:29 +0000

    sss

--- resources/js/components/TodayComponent.vue
+++ resources/js/components/TodayComponent.vue
@@ -45,17 +45,12 @@
 
                         <v-list-item>
                             <v-list-item-content>
-                                <v-list-item-title
-                                    v-text="task.title"
-                                ></v-list-item-title>
-                                <v-list-item-subtitle
-                                    v-text="task.detail"
-                                ></v-list-item-subtitle>
+                                <div class="text-justify">
+                                    {{task.title}}
+                                </div>
                                 <p class="text-justify">
-    Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.
-
-    Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.
-  </p>
+                                    {{task.detail}}
+                                </p>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list-group>
