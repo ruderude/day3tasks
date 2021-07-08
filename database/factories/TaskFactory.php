ca78fa5c764dc3660cd6d9cbd16068ffa4b77778
ruderude <rude1979@gmail.com>
2021-07-08 02:19:31 +0000

    www

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'mid' => "U4658895cbcdab9ae7e442550d350ed99",
-        'title' => $faker->title,
+        'title' => $faker->name,
         'detail' => $faker->realText,
         'done' => $faker->numberBetween($min = 0, $max = 1),
         'created_at' => $faker->datetime($max = 'now', $timezone = date_default_timezone_get()),
